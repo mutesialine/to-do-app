05f69f4af518ad7d6dc66f94bb7ed89e0abed124
Mutesi <ktesialine@gmail.com>
2023-03-15 17:36:57 +0200

    feat :  to do app all styles and add task functionality on button

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { type } from "@testing-library/user-event/dist/type";
+import { useState } from "react";
+import { AiFillDelete, AiFillPlusCircle } from "react-icons/ai";
+const App = () => {
+  const [task, setTask] = useState({
+    taskName: "",
+    isTaskDone: true,
+  });
+  const [buttonClicked, setButtonClicked]=useState(false)
+   
+
+
+const handleTask = (event) =>{
+setTask( prevTask => ({...prevTask , [event.target.name] : event.target.value}))
+}
+
+
+  return (
+    <div className="w-full max-w-7xl mx-auto mt-24 flex flex-col gap-y-8 justify-center items-center">
+      <h1 className="text-8xl font-bold text-gray-400 opacity-20">todos</h1>
+      <div className=" flex justify-between  items-center py-3 px-4 w-[50%] rounded-full  border shadow-xl ">
+        <input
+          type="text"
+          name="taskName"
+          value={task.taskName}
+          onChange={handleTask}
+          placeholder=" Add todo..."
+          className="text-xl font-semibold w-full outline-none px-4"
+        />
+        <AiFillPlusCircle
+          size={25}
+          className="text-green-800"
+          onClick={() => setButtonClicked(true)}
+        />
+      </div>
+
+      <div className="flex justify-between items-center w-[50%] border-b-2 border-gray-300 pb-2">
+        <div className="flex gap-x-3 items-center">
+          <input type="checkbox" checked={task.isTaskDone} name="isTaskDone" id="isTaskDone" />
+          {buttonClicked &&
+            <label className="text-2xl font-semibold" htmlFor="isTaskDone">
+              {task.taskName}
+            </label>
+          }
+        </div>
+        <div className="p-2 bg-gray-200 rounded-full">
+          <AiFillDelete size={16} className="text-red-500" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
